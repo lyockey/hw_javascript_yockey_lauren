508ec5731887ad37fc31bf0f6a01f34d185bf786
Lauren Yockey <laurenyockey@gmail.com>
2016-09-21 13:23:05 -0500

    Add 5 objects with president info to array

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+var presidents = [
+    {
+        firstName: 'Barack',
+        lastName: 'Obama',
+        termLength: 2,
+        party: 'Democrat',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George W',
+        lastName: 'Bush',
+        termLength: 2,
+        party: 'Republican',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'Bill',
+        lastName: 'Clinton',
+        termLength: 2,
+        party: 'Democrat',
+        yearsOfPresidency: 8
+    },
+
+    {
+        firstName: 'George',
+        lastName: 'Bush Sr',
+        termLength: 1,
+        party: 'Republican',
+        yearsOfPresidency: 4
+    },
+
+    {
+        firstName: 'Ronald',
+        lastName: 'Reagan',
+        termLength: 2,
+        party: 'Republican',
+        yearsOfPresidency: 8
+    }
+
+];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
