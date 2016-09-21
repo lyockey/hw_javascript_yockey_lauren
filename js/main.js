06eae9833573b4ba0aaec39d1da67e338f957cad
Lauren Yockey <laurenyockey@gmail.com>
2016-09-21 14:24:05 -0500

    Add numba function to count string length

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+
+    if (numba().length < 7) {
+        console.log('What a short little word!');
+    } else if (numba().length > 7) {
+        console.log("I'm sorry, that's too many to count.");
+    } else {
+        console.log('7, what a perfect choice!');
+    }
+};
+
+var string = numba('7', 'seventy', 'university');
+console.log(string);
